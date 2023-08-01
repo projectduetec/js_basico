dc42bf45e9a47346533fa33329bee2ef2cd15a6c
Maria Eduarda Batista de Oliveira <100208704+projectduetec@users.noreply.github.com>
2023-08-01 12:40:43 -0300

    Add files via upload

---
+++ funcoes/funcoes.js
@@ -0,0 +1,9 @@
+saldo = 100
+function dadosacesso(){
+    numcartão = prompt ("digite o numero de cartão")
+    senha = prompt("Digite sua senha")
+}
+function saque(valor){
+    saldo = saldo - valor
+    return saldo
+}
